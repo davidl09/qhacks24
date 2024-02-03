7d5584cfbc392b8889cd1d529c18bade965f2b44
David <dalaeer@gmail.com>
2024-02-03 14:09:36 -0500

    updated flask app

--- mainapp.py
+++ mainapp.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__, static_folder='static', static_url_path='/')
 
 
+@app.route('/')
+def home():
+    return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
